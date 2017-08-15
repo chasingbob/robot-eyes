1b9e26f6f50e0e27d36e320c7336563596b36c72
chasingbob <dries139@gmail.com>
2017-08-15 06:09:06 +0200

    added trainer skeleton

---
+++ ai/trainer.py
@@ -0,0 +1,23 @@
+'''
+Deep Learning wrapper trainer class
+
+'''
+
+
+class Trainer:
+    '''
+    Deep Learning Trainer Class
+    '''
+
+    def compile(self):
+        '''
+        Compile the Training model 
+        '''
+        pass
+    
+    def train(self):
+        '''
+        Train the model
+        '''
+        pass
+
