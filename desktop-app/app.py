72434cb4c553c6c17f67710fa4bf7a0989093361
chasingbob <dries139@gmail.com>
2017-08-15 05:53:34 +0200

    crude UX layout

--- desktop-app/app.py
+++ desktop-app/app.py
@@ -11,54 +11,25 @@ from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.properties import NumericProperty
 from kivy.properties import StringProperty
 from kivy.core.window import Window
 
+import matplotlib.pyplot as plt
+
 Builder.load_file('app.kv')
 
-class TimeWidget(Widget):
-    time = StringProperty('time')
-    date = StringProperty('date')
 
-    def update(self, *args):
-        self.date = datetime.date.today().strftime("%A, %d %B %Y")
-        self.time = datetime.datetime.now().strftime("%H:%M")
+class MainTab(TabbedPanel):
+    pass
 
-class Container(FloatLayout):
-    def __init__(self, **kwargs):
-        super(Container, self).__init__(**kwargs)
-
-        anchor_tr = AnchorLayout(anchor_x='right', anchor_y='top', size_hint=(1,1))
-        tw = TimeWidget()
-        tw.update()
-        Clock.schedule_interval(tw.update,6)
-        anchor_tr.add_widget(tw)
-        self.add_widget(anchor_tr)
-
-        anchor_ml = AnchorLayout(anchor_x='left', anchor_y='center', size_hint=(1,1))
-        btn1 = Button(text='Load class A')
-        btn1.bind(on_press=self.LoadClassA)
-    
-        btn2 = Button(text='Load class B')
-        btn2.bind(on_press=self.LoadClassB)
-
-        anchor_ml.add_widget(btn1)
-        anchor_ml.add_widget(btn2)
-        self.add_widget(anchor_ml)
-
-    def LoadClassA(self, instance):
-        print('Load class A')
-
-    def LoadClassB(self, instance):
-        print('Load class B')
-
-class DesignerApp(App):
-
+class TabbedPanelApp(App):
     def build(self):
-        cc = Container()
-        return cc
+        return MainTab()
+
 
 
 if __name__ == '__main__':
-    DesignerApp().run()
+    TabbedPanelApp().run()
